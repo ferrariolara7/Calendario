dc38333325e7ed18dd796b1ea970fb2fddb8ade6
Ferrario Lara <ferrariolara7@gmail.com>
2025-09-27 15:52:23 -0300

    Actualizo archivos modificados para reflejar cambios de navegación y strings

--- app/src/main/java/com/example/calendario/ListaMeses.kt
+++ app/src/main/java/com/example/calendario/ListaMeses.kt
@@ -1,12 +1,20 @@
 package com.example.calendario
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.appcompat.widget.Toolbar
+import android.content.Intent
+import android.widget.Button
+
+
+lateinit var toolbar: Toolbar
 
 class ListaMeses : AppCompatActivity() {
 
@@ -26,5 +34,49 @@ class ListaMeses : AppCompatActivity() {
         // 3. Configurar el Manager y el Adapter
         recyclerViewMeses.layoutManager = LinearLayoutManager(this)
         recyclerViewMeses.adapter = MesesAdapter(listaDeMeses)
+
+        toolbar = findViewById(R.id.toolbar)
+
+        setSupportActionBar(toolbar)
+        supportActionBar!!.title = resources.getString(R.string.titulo)
+
+        supportActionBar?.title = "Menú"
+
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+        val btnTerminos = findViewById<Button>(R.id.btnTerminos)
+
+        btnLogin.setOnClickListener {
+            val intent = Intent(this, logIn::class.java)
+            startActivity(intent)
+        }
+
+        btnTerminos.setOnClickListener {
+            val intent = Intent(this, TerminosActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_principal, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.item_login -> {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+            R.id.item_TermsyConds -> {
+                val intent = Intent(this, TerminosActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
