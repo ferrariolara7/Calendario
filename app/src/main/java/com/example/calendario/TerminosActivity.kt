140763e392d055998dc95172ebfed621d7c47d0f
Lara Ferrario <ferrariolara7@gmail.com>
2025-09-27 15:41:08 -0300

    Agrego nuevas clases y layouts de meses y feriados

---
+++ app/src/main/java/com/example/calendario/TerminosActivity.kt
@@ -0,0 +1,11 @@
+package com.example.calendario
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class TerminosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_terminos)
+    }
+}
